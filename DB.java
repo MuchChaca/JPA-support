518e06bbfb1535e85169fa0581c3c01417cb0dbd
Hell <bastide.seb.pro@gmail.com>
2018-04-11 15:55:26 +0400

    Added hmac func

--- DB.java
+++ DB.java
@@ -411,7 +411,35 @@ public abstract class DB {
         
         return success;
     }
-    
+    /**
+    	* HmacSHA1 - HmacSHA256
+    	* @example hmacDigest("The quick brown fox jumps over the lazy dog", "key", "HmacSHA1")
+    	* @source https://gist.github.com/MaximeFrancoeur/bcb7fc2db08c704f322a
+    */
+    public static String hmacDigest(String msg, String keyString, String algo) {
+	    String digest = null;
+	    try {
+	      SecretKeySpec key = new SecretKeySpec((keyString).getBytes("UTF-8"), algo);
+	      Mac mac = Mac.getInstance(algo);
+	      mac.init(key);
+
+	      byte[] bytes = mac.doFinal(msg.getBytes("ASCII"));
+
+	      StringBuffer hash = new StringBuffer();
+	      for (int i = 0; i < bytes.length; i++) {
+	        String hex = Integer.toHexString(0xFF & bytes[i]);
+	        if (hex.length() == 1) {
+	          hash.append('0');
+	        }
+	        hash.append(hex);
+	      }
+	      digest = hash.toString();
+	    } catch (UnsupportedEncodingException e) {
+	    } catch (InvalidKeyException e) {
+	    } catch (NoSuchAlgorithmException e) {
+	    }
+	    return digest;
+	  }
     
     
     
